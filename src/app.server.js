601327e646e02c0371edf32f867d50b644d603d3
Dennis Lee <dennis-lcl@users.noreply.github.com>
2019-07-05 09:03:12 +0800

    Add environment variable setup for production deployment.

--- src/app.server.js
+++ src/app.server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`API App Server listening on port: ${port}.`);
+  if (process.env.NODE_ENV === "production") {
+    console.log(`API App Server running on production with port: ${port}.`);
+  } else {
+    console.log(`API App Server running on localhost with port: ${port}.`);
+  }
 });
